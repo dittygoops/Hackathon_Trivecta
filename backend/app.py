5887d56eb44a5dff087042c9fb2f23472e992576
Abhave Abhilash <abhaveabhilash@gmail.com>
2024-10-26 10:18:59 -0700

    making first commit

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+# simple template for app.py
+# import the Flask class from the flask module
+from flask import Flask
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"
