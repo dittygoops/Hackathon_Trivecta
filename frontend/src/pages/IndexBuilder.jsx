90cd79dc0884c0f3a1aa9154a607e8f9ed206803
dittygoops <aditya.gup16@gmail.com>
2024-10-27 08:19:44 -0700

    IndexBuilder mostly finished

--- frontend/src/pages/IndexBuilder.jsx
+++ frontend/src/pages/IndexBuilder.jsx
@@ -1,8 +1,9 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useContext } from "react";
 
 import Header from "../components/Header";
 import StockQuerySubmission from "../components/StockQuerySubmission";
 import StockList from "../components/StockList";
+import { KeyContext } from "../App";
 
 import './IndexBuilder.css';
 
@@ -11,6 +12,9 @@ export const StockContext = createContext(null);
 const IndexBuilder = () => {
   const [stocks, setStocks] = useState([]);
   const [selectedStocks, setSelectedStocks] = useState([]);
+  const [moneyInput, setMoneyInput] = useState(0);
+  const [key, setKey] = useContext(KeyContext);
+  const [tradeResponse, setTradeResponse] = useState(''); 
 
   function reset() {
     setStocks([]);
@@ -20,6 +24,38 @@ const IndexBuilder = () => {
   function handleStocksUpdate(stocks) {
     setStocks(stocks);
   }
+
+  const handleTradeClick = async () => {
+    const tickers = selectedStocks.map(stock => stock.ticker);
+    const tradeData = {
+      tickers,
+      key: key[0],
+      secret: key[1],
+      money: moneyInput
+    };
+
+    console.log(JSON.stringify(tradeData));
+
+    // try {
+    //   const response = await fetch('http://localhost:5000/get-order', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(tradeData),
+    //   });
+
+    //   if (!response.ok) {
+    //     throw new Error('Network response was not ok');
+    //   }
+
+    //   const data = await response.json();
+    //   setTradeResponse(data.message || 'Trade successful');
+    // } catch (error) {
+    //   console.error('Error:', error);
+    //   setTradeResponse('Trade failed');
+    // }
+  };
 
   return (
     <div className="page">
@@ -34,6 +70,15 @@ const IndexBuilder = () => {
           
           <div className="right-side">
             <StockList stocks={selectedStocks}/>
+            <input
+              className={`investment-amount ${selectedStocks.length == 0 ? 'invisible' : ''}`}
+              type="number"
+              placeholder="Enter investment amount"
+              value={moneyInput}
+              onChange={(e) => setMoneyInput(e.target.value)}
+            /> 
+            <button className={`invest-button ${selectedStocks.length == 0 ? 'invisible' : ''}`} onClick={handleTradeClick} disabled={moneyInput.length == 0 ? 'disabled' : ''}>Invest</button>
+            <div className={`trade-response ${selectedStocks.length == 0 ? 'invisible' : ''}`}>{tradeResponse}</div>
           </div>
         </div>
       </StockContext.Provider>
